5079acd4e4c98891635062631c13c87318745b71
Henry Rausch <github@simple.postrausch.tech>
2026-02-27 01:36:12 +0100

    models: add circularity hub structures

---
+++ models/circularity_hub.go
@@ -0,0 +1,38 @@
+package models
+
+// FilterObject represents a filter/sort payload for CircularityHub POST-body
+// filter endpoints such as suggest-category.
+type FilterObject struct {
+	Filter map[string]map[FilterOperator]any `json:"filter,omitempty"`
+	Sort   map[string]SortDirection          `json:"sort,omitempty"`
+}
+
+// AddObjectEntry describes an object to be added to the CircularityHub.
+type AddObjectEntry struct {
+	Category string `json:"category"`
+	Price    string `json:"price"`
+}
+
+// CircularityHubBillingData holds billing address details for a CircularityHub order.
+type CircularityHubBillingData struct {
+	FirstName   *string `json:"first_name"`
+	LastName    *string `json:"last_name"`
+	Street      *string `json:"street"`
+	HouseNumber *string `json:"house_number"`
+	ZipCode     *string `json:"zip_code"`
+	City        *string `json:"city"`
+}
+
+// CircularityHubOrder represents an order in the CircularityHub module.
+type CircularityHubOrder struct {
+	ID                 int                        `json:"id"`
+	OrderNumber        string                     `json:"order_number"`
+	CreatedAt          string                     `json:"created_at"`
+	UserID             *int                       `json:"user_id"`
+	TotalPrice         *string                    `json:"total_price"`
+	Completed          bool                       `json:"completed"`
+	Cancelled          bool                       `json:"cancelled"`
+	CancellationReason *string                    `json:"cancellation_reason"`
+	BillingData        *CircularityHubBillingData `json:"billing_data"`
+	Articles           []map[string]any           `json:"articles"`
+}
